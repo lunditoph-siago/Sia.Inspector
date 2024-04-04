684d490914ba4741893bb0168adce31c1a0ed469
Phlam Sicusa <sicusa@gilatod.art>
2024-04-05 00:34:49 +0800

    fix ordering of host components

--- Sia.Inspector.API/Responses/Extensions/EntityDescriptorExtensions.cs
+++ Sia.Inspector.API/Responses/Extensions/EntityDescriptorExtensions.cs
@@ -3,5 +3,5 @@ namespace Sia.Inspector.API.Reponses;
 public static class EntityDescriptorExtensions
 {
     public static IEnumerable<string> GetComponentsResponse(this EntityDescriptor descriptor)
-        => descriptor.Offsets.Keys.Select(type => type.ToString());
+        => descriptor.Components.Select(info => info.Type.ToString());
 }
